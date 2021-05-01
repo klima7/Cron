f4ea33358cbcbee9e0af734c59e9d7fd9a7f2bab
Lukasz Klimkiewicz <ukasz.klimkiewicz2@wp.pl>
2021-05-01 10:15:34 +0200

    server file removed

--- cron.cpp
+++ cron.cpp
@@ -1,13 +1,16 @@
 #include "cron.h"
-#include "server.h"
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
 using namespace std;
 
+void start_server();
+string execute_command(string command);
+void handle_connection(int server_sock);
 string connect_arguments(int argc, char **argv);
 string send_to_server(string command);
 
@@ -43,6 +46,79 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+void start_server() {
+
+    // Create socket
+    int server_sock = socket(AF_INET , SOCK_STREAM , 0);
+    if(server_sock == -1)
+        throw exception();
+
+    int iSetOption = 1;
+    int res = setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (char*)&iSetOption, sizeof(iSetOption));
+    if(res < 0)
+        throw exception();
+
+    // Bind
+    struct sockaddr_in server_addr = {0};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    res = bind(server_sock, (struct sockaddr *)&server_addr , sizeof(server_addr));
+    if(res < 0)
+        throw cron_running_exception();
+
+    // Listen
+    res = listen(server_sock, 10);
+    if(res < 0)
+        throw exception();
+
+    cout << "Cron is running" << endl;
+    while(true) {
+        handle_connection(server_sock);
+    }
+}
+
+void handle_connection(int server_sock) {
+
+    // Accept
+    sockaddr_in client_addr;
+    socklen_t addrlen = sizeof(client_addr);
+    int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addrlen);
+    if (client_sock<0) {
+        cout << "Error occured on acception connection" << endl;
+        return;
+    }
+
+    // Read command
+    char response_buff[2000];
+    int res = recv(client_sock, response_buff , 2000 , 0);
+    if(res < 0) {
+        cout << "Error occured on reading command" << endl;
+        close(client_sock);
+        return;
+    }
+    string command = string(response_buff, res);
+
+    // Execute command
+    string reply = execute_command(command);
+
+    // Send reply
+    res = send(client_sock , reply.c_str() , reply.size() , 0);
+    if(res < 0) {
+        cout << "Error occured on sending reply" << endl;
+        close(client_sock);
+        return;
+    }
+
+    // Close connection
+    close(client_sock);
+}
+
+string execute_command(string command) {
+    return "Command added\n";
+}
+
 string connect_arguments(int argc, char **argv) {
     stringstream ss;
     for(int i=1; i<argc; i++) {
@@ -63,7 +139,7 @@ string send_to_server(string command) {
     // Connect socket
     struct sockaddr_in server = {0};
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_port = htons(PORT);
 
     int res = connect(socket_desc ,(struct sockaddr *)&server , sizeof(server));
--- server.h
+++
@@ -1,6 +0,0 @@
-#ifndef CRON_SERVER_H
-#define CRON_SERVER_H
-
-void start_server();
-
-#endif
