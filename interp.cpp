80e53fe364db6c68126701feda4a413577a7e024
Lukasz Klimkiewicz <ukasz.klimkiewicz2@wp.pl>
2021-05-01 13:19:47 +0200

    Empty commands added

--- interp.cpp
+++ interp.cpp
@@ -7,5 +7,37 @@ Interpreter::Interpreter(Cron cron) {
 }
 
 string Interpreter::interpret(std::string command) {
-    return "Command interpreted";
+
+    vector<string> tokens = get_tokens(command, " ");
+
+    if(tokens[0] == "add") {
+        return "Task added";
+    }
+    else if(tokens[0] == "remove") {
+        return "Task removed";
+    }
+    else if(tokens[0] == "exit") {
+        return "Cron exited";
+    }
+    else if(tokens[0] == "list") {
+        return "Listing tasks";
+    }
+    else {
+        return "Invalid command";
+    }
+}
+
+vector<string> Interpreter::get_tokens(string text, string delimiter) {
+    vector<string> tokens;
+    size_t pos = 0;
+    string token;
+
+    while ((pos = text.find(delimiter)) != string::npos) {
+        token = text.substr(0, pos);
+        tokens.push_back(token);
+        text.erase(0, pos + delimiter.length());
+    }
+    tokens.push_back(text);
+
+    return tokens;
 }
--- interp.h
+++ interp.h
@@ -2,6 +2,7 @@
 #define CRON_INTERP_H
 
 #include <string>
+#include <vector>
 #include "cron.h"
 
 
@@ -10,6 +11,7 @@ public:
     Interpreter(Cron cron);
     std::string interpret(std::string command);
 private:
+    static std::vector<std::string> get_tokens(std::string text, std::string delimiter);
     Cron cron;
 };
 
