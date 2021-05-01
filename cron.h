0903b87d3fe73bd9f8f10cf921f2b7b5b32df1e4
Lukasz Klimkiewicz <ukasz.klimkiewicz2@wp.pl>
2021-05-01 13:24:58 +0200

    Cron interface created

--- cron.h
+++ cron.h
@@ -2,9 +2,14 @@
 #define CRON_CRON_H
 
 #include <string>
+#include <list>
+#include "task.h"
 
 class Cron {
-
+    void add_task(Task task);
+    void remove_task(int task_id);
+    void exit();
+    std::list<Task> get_tasks();
 };
 
 
--- task.h
+++ task.h
@@ -1,6 +1,8 @@
 #ifndef CRON_TASK_H
 #define CRON_TASK_H
 
+class Task {
 
+};
 
 #endif
