31ea02a51c4fa04044d51e97e8565a0b77b8953e
Lukasz Klimkiewicz <ukasz.klimkiewicz2@wp.pl>
2021-05-01 10:25:22 +0200

    Separate classes for client and server

---
+++ connect.h
@@ -0,0 +1,28 @@
+#ifndef CRON_CONNECT_H
+#define CRON_CONNECT_H
+
+#include <stdexcept>
+
+#define PORT 6438
+
+class cron_running_exception: public std::exception {};
+
+class cron_not_running_exception: public std::exception {};
+
+class server {
+public:
+    void start_server();
+private:
+    void handle_connection(int server_sock);
+    std::string execute_command(std::string command);
+};
+
+class client {
+public:
+    std::string execute_command(int argc, char **argv);
+private:
+    std::string connect_arguments(int argc, char **argv);
+    std::string send_to_server(std::string command);
+};
+
+#endif
