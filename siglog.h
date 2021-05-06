a7c9868d5ff7224ca1e5bb1d405cc5c97b4bd0f5
Lukasz Klimkiewicz <ukasz.klimkiewicz2@wp.pl>
2021-05-06 11:34:02 +0200

    Siglog library added and translated to cpp

---
+++ siglog.h
@@ -0,0 +1,27 @@
+#ifndef UNTITLED2_SIGLOG_H
+#define UNTITLED2_SIGLOG_H
+
+namespace siglog {
+
+    // Types
+    enum siglog_level_t {
+        SIGLOG_DISABLED, SIGLOG_MAX, SIGLOG_STANDARD, SIGLOG_MIN
+    };
+    typedef enum siglog_level_t SIGLOG_LEVEL;
+    typedef void (*DUMP_FUNCTION)(FILE *file);
+
+    // Lifecycle functions
+    int siglog_init(int level_sig, int dump_sig, SIGLOG_LEVEL level, char *dir_path);
+    void siglog_free();
+
+    // Logging functions
+    void siglog_log(SIGLOG_LEVEL level, char *fmt, ...);
+    void siglog_min(char *fmt, ...);
+    void siglog_standard(char *fmt, ...);
+    void siglog_max(char *fmt, ...);
+
+    // Dumping functions
+    int siglog_register_dump_function(DUMP_FUNCTION fun);
+}
+
+#endif
